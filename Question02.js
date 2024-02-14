4f07281371f79187f089b06f3f24e4577a11ec51
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-02-14 17:24:41 +0530

    made some small modification

--- Question02.js
+++ Question02.js
@@ -2,5 +2,5 @@
 
 const readLine = require('readline-sync');
 let num1 = parseInt(readLine.question("Enter the number 1\n"));
-let num2 = parseInt(readLine.question("Enter the number 2\n"));
+let num2 = parseFloat(readLine.question("Enter the number 2\n"));
 console.log(`The sum of the two numbers are ${num1+num2}.`)
